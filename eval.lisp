d03b6be8b48f618bd39aaf173f6f7c91f874454b
dokudami <ysdzm75@gmail.com>
2025-06-24 15:41:56 +0900

    fix: 自己完結するように更新

--- eval.lisp
+++ eval.lisp
@@ -1,57 +1,43 @@
-(defun list_ (x y)
-  (cons x (cons y '())))
+(let ((env nil)) ; ← 初期環境は空
+  (labels
+      ((eval_ (e)
+         (cond
+           ((or (numberp e) (stringp e) (characterp e)) e)
 
-(defun assoc_ (x y)
-  (cond ((eq (caar y) x) (cadar y))
-        (t (assoc_ x (cdr y)))))
+           ((symbolp e)
+            (let ((val (cdr (assoc e env))))
+              (eval_ val)))
 
-(defun pair_ (x y)
-  (cond ((and (null x) (null y)) '())
-        ((and (not (atom x)) (not (atom y)))
-         (cons (list_ (car x) (car y)) (pair_ (cdr x) (cdr y))))))
+           ((and (consp e) (eq (car e) 'quote))
+            (cadr e))
 
-(defun evlis_ (m a)
-  (cond ((null m) '())
-        (t (cons (eval_ (car m) a) (evlis_ (cdr m) a)))))
+           ((and (consp e) (eq (car e) 'print))
+            (apply #'print (mapcar #'eval_ (cdr e))))
 
-(defun evcon_ (c a)
-  (cond ((eval_ (caar c) a) (eval_ (cadar c) a))
-        (t (evcon_ (cdr c) a))))
+           ((and (consp e) (eq (car e) '+))
+            (apply #'+ (mapcar #'eval_ (cdr e))))
 
-(defun eval_ (e a)
-  (cond
-    ((atom e)
-     (if (or (numberp e) (stringp e) (characterp e) (eq e t) (eq e nil))
-         e
-         (assoc_ e a)))
-    ((atom (car e))
-     (cond
-       ((eq (car e) 'quote) (cadr e))
-       ((eq (car e) 'atom)  (atom (eval_ (cadr e) a)))
-       ((eq (car e) 'eq)    (eq (eval_ (cadr e) a)
-                                (eval_ (caddr e) a)))
-       ((eq (car e) 'car)   (car (eval_ (cadr e) a)))
-       ((eq (car e) 'cdr)   (cdr (eval_ (cadr e) a)))
-       ((eq (car e) 'cons)  (cons (eval_ (cadr e) a)
-                                  (eval_ (caddr e) a)))
-       ((eq (car e) 'cond)  (evcon_ (cdr e) a))
-       ((eq (car e) 'print) (print (eval_ (cadr e) a)))
-       ((eq (car e) 'format)
-        (apply #'format (mapcar (lambda (x) (eval_ x a)) (cdr e))))
-       (t (eval_ (cons (assoc_ (car e) a) (cdr e)) a))))
-    ((eq (caar e) 'lambda)
-     (eval_ (caddar e)
-            (append (pair_ (cadar e) (evlis_ (cdr e) a)) a)))
-    ((eq (caar e) 'label)
-     (eval_ (cons (caddar e) (cdr e))
-            (cons (list_ (cadar e) (car e)) a)))))
+           ((and (consp e) (eq (car e) '*))
+            (apply #'* (mapcar #'eval_ (cdr e))))
 
-(defun run-my-program (filename)
-  (unless filename
-    (error "ファイル名を指定してください。"))
+           ((and (consp e) (eq (car e) 'cons))
+            (apply #'cons (mapcar #'eval_ (cdr e))))
 
-  (with-open-file (in filename)
-    (loop for expr = (read in nil :eof)
-          while (not (eq expr :eof))
-          do
-            (eval_ expr '()))))
+           ((and (consp e) (eq (car e) 'eval_))
+            (apply #'eval_ (mapcar #'eval_ (cdr e))))
+
+           (t (error "Unknown expression: ~S" e)))))
+    
+    ;; 環境に hello と eval_ を登録
+    (setf env
+          (list
+           (cons 'hello '(print (+ 1 (* 3 3)))) ; → 10
+           (cons 'eval_ #'eval_)))
+    
+    ;; 1回評価（直接）
+    (eval_ 'hello) ; → 10 を出力
+    
+    (eval_ '(eval_ '(eval_ '(eval_ 'hello))))
+
+    ;; 2回評価（入れ子）
+    (eval_ '(eval_ 'hello)))) ; → 同じく 10 を出力
